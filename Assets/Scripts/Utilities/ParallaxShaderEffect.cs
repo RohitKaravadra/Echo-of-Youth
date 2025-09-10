4c1c84987cef85775b66e9b31b730894f45ee96e
DESKTOP-M39FV4S\Rohit Karavadra <rohitkaravadra1997@gmail.com>
2025-09-10 17:22:15 +0100

    Parallax Shader Bug Fix

--- Assets/Scripts/Utilities/ParallaxShaderEffect.cs
+++ Assets/Scripts/Utilities/ParallaxShaderEffect.cs
@@ -3,21 +3,48 @@ using UnityEngine;
 public class ParallexShaderEffect : MonoBehaviour
 {
     [SerializeField] Vector2 _ScrollSpeed;
-    [SerializeField] bool _FollowCameraX;
-    [SerializeField] bool _FollowCameraY;
+    [SerializeField] Vector2 _Offset;
+    [SerializeField] float _Scale = 1f;
+    [SerializeField] bool _WrapX = true;
+    [SerializeField] bool _WrapY = true;
 
     SpriteRenderer _Renderer;
 
     Vector2 _LastCamPos;
     Transform _Camera;
-    Vector2 _Offset;
+    bool _HasRenderer = false;
+
+    private void OnValidate()
+    {
+        if (_Renderer == null)
+            _Renderer = GetComponent<SpriteRenderer>();
+        if (_Renderer != null)
+        {
+            _Renderer.sharedMaterial.SetFloat("_Scale", _Scale);
+            _Renderer.sharedMaterial.SetVector("_Offset", _Offset);
+            _Renderer.sharedMaterial.SetInt("_Wrap_X", _WrapX ? 1 : 0);
+            _Renderer.sharedMaterial.SetInt("_Wrap_Y", _WrapY ? 1 : 0);
+        }
+    }
 
     private void Awake()
     {
         _Camera = Camera.main.transform;
-        _Renderer = GetComponent<SpriteRenderer>();
+
+        if (_Renderer == null)
+            _HasRenderer = TryGetComponent(out _Renderer);
+        else
+            _HasRenderer = true;
 
         _LastCamPos = _Camera.position;
+
+        if (_HasRenderer)
+        {
+            _Renderer.material.SetFloat("_Scale", _Scale);
+            _Renderer.material.SetVector("_Offset", _Offset);
+            _Renderer.material.SetInt("_Wrap_X", _WrapX ? 1 : 0);
+            _Renderer.material.SetInt("_Wrap_Y", _WrapY ? 1 : 0);
+        }
     }
 
     private void Update()
@@ -27,21 +54,18 @@ public class ParallexShaderEffect : MonoBehaviour
             Vector2 newCamPos = _Camera.position;
             Vector2 offset = newCamPos - _LastCamPos;
 
-            _LastCamPos = newCamPos;
-
-            Vector2 newPos = transform.position;
-
-            if (_FollowCameraX)
-                newPos.x = _Camera.position.x;
-            if (_FollowCameraY)
-                newPos.y = _Camera.position.y;
-
-            transform.position = newPos;
-
-            if (offset.magnitude < 0.5f)
+            if (offset.magnitude > 0.01f)
             {
+                _LastCamPos = newCamPos;
                 _Offset += offset * _ScrollSpeed;
-                _Renderer.material.SetVector("_Offset", _Offset);
+
+                if (_HasRenderer)
+                {
+                    _Renderer.material.SetFloat("_Scale", _Scale);
+                    _Renderer.material.SetVector("_Offset", _Offset);
+                    _Renderer.material.SetInt("_Wrap_X", _WrapX ? 1 : 0);
+                    _Renderer.material.SetInt("_Wrap_Y", _WrapY ? 1 : 0);
+                }
             }
         }
     }
